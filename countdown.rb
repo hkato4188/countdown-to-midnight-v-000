5afd3c70b61bbf3c7ac135569d2b89c29bb68e91
Hiroki Kato <hkato@sas.upenn.edu>
2018-02-08 20:59:02 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -4,11 +4,13 @@ def countdown(x)
     while x > 0
       puts "#{x} SECOND(S)!"
         x -= 1
+    countdown_with_sleep
     end
   puts "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(n)
+  n = 1
   while n > 0
   sleep 1
   n -= 1
