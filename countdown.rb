b6b6c0140e6c3d12cd2371bcbd37fde1c4511f46
Hiroki Kato <hkato@sas.upenn.edu>
2018-02-08 20:46:07 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,13 @@
 #write your code here
 
-def countdown
+def countdown(x)
+    x = x.to_i
+
+    while x > 0
+      puts "#{x} SECOND(S)!"
+        x -= 1
+    end
+  puts "HAPPY NEW YEAR!"
 end
+
+countdown
